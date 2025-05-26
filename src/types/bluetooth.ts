f89213cd56a1f0b99428e383545e1be42298e049
ehowlfl <75974676+ehowlfl@users.noreply.github.com>
2025-05-27 07:47:19 +0900

    A

--- src/types/bluetooth.ts
+++
@@ -1,9 +0,0 @@
-export interface SerialPortInfo {
-  path: string;
-  manufacturer?: string;
-  serialNumber?: string;
-  pnpId?: string;
-  locationId?: string;
-  vendorId?: string;
-  productId?: string;
-}
